5c6f1a61370fd6a8523cf0fef2b46ce02e7d0312
mohamed-elfiky <elfikymohamedelfiky@gmail.com>
2019-04-27 04:29:03 +0200

    modified OCR

---
+++ backend/OCR/util.py
@@ -0,0 +1,139 @@
+from imutils.object_detection import non_max_suppression
+import numpy as np
+import pytesseract
+import argparse
+import cv2 as cv
+from matplotlib import pyplot as plt
+def decode_predictions(scores, geometry, min_confidence):
+	# grab the number of rows and columns from the scores volume, then
+	# initialize our set of bounding box rectangles and corresponding
+	# confidence scores
+	(numRows, numCols) = scores.shape[2:4]
+	rects = []
+	confidences = []
+
+	# loop over the number of rows
+	for y in range(0, numRows):
+		# extract the scores (probabilities), followed by the
+		# geometrical data used to derive potential bounding box
+		# coordinates that surround text
+		scoresData = scores[0, 0, y]
+		xData0 = geometry[0, 0, y]
+		xData1 = geometry[0, 1, y]
+		xData2 = geometry[0, 2, y]
+		xData3 = geometry[0, 3, y]
+		anglesData = geometry[0, 4, y]
+
+		# loop over the number of columns
+		for x in range(0, numCols):
+			# if our score does not have sufficient probability,
+			# ignore it
+			if scoresData[x] < min_confidence:
+				continue
+
+			# compute the offset factor as our resulting feature
+			# maps will be 4x smaller than the input image
+			(offsetX, offsetY) = (x * 4.0, y * 4.0)
+
+			# extract the rotation angle for the prediction and
+			# then compute the sin and cosine
+			angle = anglesData[x]
+			cos = np.cos(angle)
+			sin = np.sin(angle)
+
+			# use the geometry volume to derive the width and height
+			# of the bounding boxps
+			h = xData0[x] + xData2[x]
+			w = xData1[x] + xData3[x]
+
+			# compute both the starting and ending (x, y)-coordinates
+			# for the text prediction bounding box
+			endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+			endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+			startX = int(endX - w)
+			startY = int(endY - h)
+
+			# add the bounding box coordinates and probability score
+			# to our respective lists
+			rects.append((startX, startY, endX, endY))
+			confidences.append(scoresData[x])
+
+	#return a tuple of the bounding boxes and associated confidences
+	return (rects, confidences)
+######################################################################################################################
+######################################################################################################################
+#revert the bounding boxes back to their original sizes 
+def OrigBoxes(boxes, rW, rH, origW, origH, padding,stage = 1):
+	# initialize lists fo results , sizes
+	bboxes = []
+	# loop over the bounding boxes
+	for (startX, startY, endX, endY) in boxes:
+		# scale the bounding box coordinates based on the respective
+		# ratios
+		startX = int(startX * rW)
+		startY = int(startY * rH)
+		endX = int(endX * rW)
+		endY = int(endY * rH)
+		# in order to obtain a better OCR of the text we can potentially
+		# apply a bit of padding surrounding the bounding box -- here we
+		# are computing the deltas in both the x and y directions
+		dX = int((endX - startX) * padding)
+		dY = int((endY - startY) * padding)
+		# apply padding to each side of the bounding box, respectively
+		if stage == 1 : 
+			startX = max(0, startX - dX)
+			startY = max(0, startY - dY)
+			endX = min(origW, endX + (dX * 2))
+			endY = min(origH, endY + (dY * 2))
+		if stage == 2 :
+			startX = max(0, startX)
+			startY = max(0, startY - dY)
+			endX = min(origW, endX)
+			endY = min(origH, endY + (dY * 2))
+
+		
+		size = abs(endY-startY) * abs(endX-endY)
+		bboxes.append(((startX, startY, endX, endY), size))
+	return bboxes
+
+######################################################################################################################
+######################################################################################################################
+#get bounding boxes from East detector
+def boundingBoxes(image, newW, newH, east, min_confidence):	
+	# load the input image and grab the image dimensions
+	orig = image.copy()
+	(origH, origW) = image.shape[:2]
+
+	# set the new width and height and then determine the ratio in change
+	# for both the width and height
+	#(newW, newH) = (args["width"], args["height"])
+	rW = origW / float(newW)
+	rH = origH / float(newH)
+
+	# resize the image and grab the new image dimensions
+	image = cv.resize(image, (newW, newH))
+	(H, W) = image.shape[:2]
+
+	# define the two output layer names for the EAST detector model that
+	# we are interested -- the first is the output probabilities and the
+	# second can be used to derive the bounding box coordinates of text
+	layerNames = [
+		"feature_fusion/Conv_7/Sigmoid",
+		"feature_fusion/concat_3"]
+
+	# load the pre-trained EAST text detector
+	print("[INFO] loading EAST text detector...")
+	net = cv.dnn.readNet(east)
+
+	# construct a blob from the image and then perform a forward pass of
+	# the model to obtain the two output layer sets
+	blob = cv.dnn.blobFromImage(image, 1.0, (W, H),
+		(123.68, 116.78, 103.94), swapRB=True, crop=False)
+	net.setInput(blob)
+	(scores, geometry) = net.forward(layerNames)
+
+	# decode the predictions, then  apply non-maxima suppression to
+	# suppress weak, overlapping bounding boxes
+	(rects, confidences) = decode_predictions(scores, geometry, min_confidence)
+	boxes = non_max_suppression(np.array(rects), probs=confidences)
+	return boxes, rH, rW, origH, origW
